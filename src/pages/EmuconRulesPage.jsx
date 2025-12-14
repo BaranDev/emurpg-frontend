054dbcf0ed750c7b11556a3841487f349f744c9e
Cevdet Baran Oral <cevdetbaranoral@gmail.com>
2025-12-14 13:09:35 +0200

    fix

--- src/pages/EmuconRulesPage.jsx
+++ src/pages/EmuconRulesPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { FaScroll, FaGlobe, FaArrowLeft } from "react-icons/fa";
 import PropTypes from "prop-types";
